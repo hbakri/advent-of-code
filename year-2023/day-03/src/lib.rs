8955545a9bfd72e55173c621928d255fbee27b65
Hicham Bakri <hicham.bakri@picsellia.com>
2023-12-06 23:12:00 +0100

    2023: day 03 part 1 refactor

--- year-2023/day-03/src/lib.rs
+++ year-2023/day-03/src/lib.rs
@@ -1,43 +1,34 @@
-use std::cmp;
 use std::fs;
 
 fn part1(input: &str) -> i32 {
     let mut part_number_sum = 0;
+
     input.lines().enumerate().for_each(|(index, line)| {
-        let mut mutable_line = line;
-        while let Some(number_start_index) = mutable_line.find(|c: char| c.is_numeric()) {
-            let number_end_index = mutable_line[number_start_index..].find(|c: char| !c.is_numeric()).unwrap_or(mutable_line.len() - number_start_index);
-            let number_string = &mutable_line[number_start_index..number_start_index + number_end_index];
+        let mut current_index = 0;
 
-            let diff: usize = line.len() - mutable_line.len();
-            let search_start_index: usize = cmp::max((number_start_index + diff) as i32 - 1, 0) as usize;
-            let search_end_index: usize = cmp::min((number_start_index + number_end_index) + diff + 1, line.len());
-            let neighbor_range = search_start_index..search_end_index;
-
-            let mut is_part_number = false;
+        while let Some(relative_number_start_index) = line[current_index..].find(|c: char| c.is_numeric()) {
+            let number_start_index = current_index + relative_number_start_index;
+            let number_string = line[number_start_index..]
+                .chars()
+                .take_while(|c| c.is_numeric())
+                .collect::<String>();
             let number = number_string.parse::<i32>().unwrap();
 
-            if let Some(previous_line) = input.lines().nth((index as i32 - 1) as usize) {
-                if previous_line[neighbor_range.clone()].contains(|c: char| !c.is_numeric() && c != '.') {
-                    is_part_number = true;
-                }
-            }
+            let horizontal_check_range = number_start_index.saturating_sub(1)..(number_start_index + number_string.len() + 1).min(line.len());
+            let vertical_check_range = index.saturating_sub(1)..=(index + 1).min(input.lines().count());
+            let is_adjacent_to_symbol = input.lines()
+                .enumerate()
+                .filter(|&(index, _)| vertical_check_range.contains(&index))
+                .any(|(_, neighbor_line)| {
+                    neighbor_line.get(horizontal_check_range.clone())
+                        .map_or(false, |segment| segment.chars().any(|c| !c.is_numeric() && c != '.'))
+                });
 
-            if line[neighbor_range.clone()].contains(|c: char| !c.is_numeric() && c != '.') {
-                is_part_number = true;
-            }
-
-            if let Some(next_line) = input.lines().nth(index + 1) {
-                if next_line[neighbor_range.clone()].contains(|c: char| !c.is_numeric() && c != '.') {
-                    is_part_number = true;
-                }
-            }
-
-            if is_part_number {
+            if is_adjacent_to_symbol {
                 part_number_sum += number;
             }
 
-            mutable_line = &mutable_line[number_start_index + number_end_index..];
+            current_index = number_start_index + number_string.len();
         }
     });
     return part_number_sum;
